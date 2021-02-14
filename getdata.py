20fa7cb62036fb4d16e225867889bba51d0ef931
tatsuyanishimura <78542606+tatsuya@users.noreply.github.com>
2021-02-14 21:03:45 +0900

    レイアウト調整
    営業時間の表示調整、重複の削除

--- getdata.py
+++ getdata.py
@@ -2,28 +2,29 @@ import requests
 
 
 class Gourmet:  #グルメサーチAPI
+    def __init__(self):
+        self.day = ['月', '火', '水', '木', '金', '土', '日']
+        self.num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+        self.req_data = {}
 
     def set(self, lat, lng, ran):
-        self.lat = lat
-        self.lng = lng
-        self.ran = ran
-        self.req_data = self.get()
+        self.req_data = self.get(lat,lng,ran)
 
-    def get(self) -> object:
+    def get(self,lat,lng,ran) -> object:
         url = 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=77c25fbd1899d0c3'
         url = url + '&format=json&count=100'  # json形式でデータ取得、最大100件表示
-        url = url + '&lat=' + self.lat + '&lng=' + self.lng + '&range=' + self.ran  # 範囲設定（lat:緯度、lng:経度）
+        url = url + '&lat=' + lat + '&lng=' + lng + '&range=' + ran  # 範囲設定（lat:緯度、lng:経度、ran:範囲）
         return requests.get(url).json()
 
     def cnt_data(self):
         try:
-            return self.req_data['results']['results_available']
+            return int(self.req_data['results']['results_available'])
         except:
-            print('cnt_data error')
             return 0
 
     def shop_data(self):
         data_names = ['name', 'address', 'access', 'photos', 'photol', 'budget','open']
+
         data = []
         try:
             for shop_data in self.req_data['results']['shop']:
@@ -35,27 +36,28 @@ class Gourmet:  #グルメサーチAPI
                         shop_x['photol'] = shop_data['photo']['pc']['l']
                     elif data_name is 'budget':
                         shop_x['budget'] = shop_data['budget']['name']
+                    elif data_name is 'open':
+                        str = ''
+                        chr2 = ''
+                        for chr in shop_data['open']:
+                            if self.open_remaker(chr,chr2):
+                                str = str + '<br>'
+                            chr2 = chr
+                            str = str + chr
+                        shop_x['open'] = str
                     else:
                         shop_x[data_name] = shop_data[data_name]
                 data.append(shop_x)
         except:
-            print('cnt error')
+            pass
         return data
 
+    def open_remaker(self, chr, chr2):
+        if chr2 in self.num and chr in self.day:
+            return True
+        else:
+            return False
 
-
-    def get_data_test(self, data_names):
-
-        data = []
-        for shop in self.req_data['results']['shop']:
-            shop_data = {}
-            for data_name in data_names:
-                if data_name is 'photo':
-                    shop_data[data_name] = shop['photo']['pc']['l']
-                else:
-                    shop_data[data_name] = shop[data_name]
-            data.append(shop_data)
-        return data
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Markup
 from getdata import Gourmet
 import math
 import webbrowser
