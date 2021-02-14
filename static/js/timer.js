cee2064d9a473eed6330a3c321644ab982313016
tatsuyanishimura <78542606+tatsuya@users.noreply.github.com>
2021-02-14 17:53:18 +0900

    レイアウト変更
    いくつかの<a>タグのレイアウトを変更

--- static/js/timer.js
+++ static/js/timer.js
@@ -1,15 +1,26 @@
-//曜日名を得る関数
+
+clock();//時計の初期化
+setInterval(clock, 1000);//時計の更新
+
+//曜日を表示する関数
 function renderDay(i) {
     return ['日', '月', '火', '水', '木', '金', '土'][i];
 }
-//  毎分呼び出される関数
-function clock() {
 
-    var timer = document.getElementsByClassName('timer');
-    var now = new Date();
-
-    //曜日と時刻を表示
-    timer.textContent = renderDay(now.getDay()) + '|' + now.getMinutes() +　':' + now.getHours();
+//十の位を表示
+function set2fig(num) {
+   var ret;
+   if( num < 10 ) { ret = "0" + num; }
+   else { ret = num; }
+   return ret;
 }
-//起動時に実行するプログラム
-setInterval(clock, 60000);
+
+//時計の表示と更新をする関数
+function clock() {
+    var timer = document.getElementById('timer');
+    var now = new Date();
+    var hour = set2fig(now.getHours())
+    var minute = set2fig(now.getMinutes())
+    timer.innerHTML = '現在<br>'+renderDay(now.getDay()) + '曜日<br>' + hour +　':' + minute;
+}
+
